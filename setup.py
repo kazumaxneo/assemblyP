ccc8c9d39bc17188396017b4d831405d36cdb05e
uesaka <kazumaxneo@gmail.com>
2021-04-23 11:20:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,43 +1,29 @@
 #!/usr/bin/env python3
-"""
-This is  
-"""
-
-import os
-import shutil
-import sys
 
 from setuptools import setup
-from setuptools.command.install import install
+import glob
+
+scripts = glob.glob("*.p*")
+
+setup(
+    name='assemblyP',
+    description='assemblyP: a tool for designing primer at the end of contig.',
+    author='Kazuma Uesaka',
+    author_email='kazumaxneo@gmail.com',
+    packages=['assemblyP'],
+    license='GPLv3',
+    url='https://github.com/kazumax',
+    package_dir={'assemblyP': 'assemblyP/'},
+    entry_points={"console_scripts": ['assemblyP = assemblyP.__main__:main']},
+    python_requires='>=3.6',
+    install_requires=[
+              'biopython',
+          ],
+    scripts=scripts,
+    zip_safe=True
+)
 
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
 
 
-# Get the program version from another file.
-__version__ = '0.3.0'
-exec(open('assemblyP/version.py').read())
-
-def parse_requirements():
-    """ load requirements from a pip requirements file """
-    lineiter = (line.strip() for line in open('requirements.txt'))
-    return [line for line in lineiter if line and not line.startswith("#")]
-
-setup(name='assemblyP',
-      version=__version__,
-      description='assemblyP: a tool for designing primer at the end of contig',
-      long_description=readme(),
-      long_description_content_type='text/markdown',
-      url='https://github.com/kazumax',
-      author='Kazuma Uesaka',
-      author_email='kazumax@gmail.com',
-      license='GPLv3',
-      packages=['assemblyP'],
-      install_requires=parse_requirements(),
-      entry_points={"console_scripts": ['assemblyP = assemblyP.__main__:main']},
-      include_package_data=True,
-      zip_safe=False,
-      python_requires='>=3.6')
 
